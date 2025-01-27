5148931805364ed7e9eb64814e15297da6aff585
k-hroma <rociomendonca@gmail.com>
2025-01-27 11:16:11 -0300

    for each

--- 25-IterarArrays.js
+++ 25-IterarArrays.js
@@ -5,6 +5,14 @@ let numerosArreglo = [13, 21, 0, 0, 62];
 
 // Leer los valores
 console.log(numerosArreglo.length);
-for(let i = 0; i < numerosArreglo.length; i++){
-    console.log(`Arreglo[${i}] = ${numerosArreglo[i]}`);
+for (let i = 0; i < numerosArreglo.length; i++) {
+  console.log(`Arreglo[${i}] = ${numerosArreglo[i]}`);
 }
+
+// forEach:
+
+// Es un método específico de los arrays que toma una función como argumento y ejecuta esa función una vez por cada elemento del array.
+// Ejemplo
+array.forEach((element) => {
+  console.log(element);
+});
